c15910ac0a80c20662c2c569199f4f4830cb4aaf
christian <chrisstianandres@gmail.com>
2021-03-15 21:39:32 -0500

    Version 1

--- citas/urls.py
+++ citas/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.urls import path
+
+from apps import backEnd
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('menu', login_required(backEnd.menu), name='menu'),
 ]
